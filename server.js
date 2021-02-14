802640394944bf09d00dd52896adc94294edad55
Eric Hunter <ehunter7@live.com>
2021-02-14 12:07:16 -0800

    adding add functions

--- server.js
+++ server.js
@@ -9,6 +9,23 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const connection = mysql.createConnection({
+  host: `localhost`,
+  port: 3306,
+  user: `root`,
+  password: `Emmamarie1`,
+  database: `EmployeeManagement_db`,
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error(`error connecting`);
+    return;
+  }
+
+  console.log(`connected as id ${connection.threadId}`);
+});
+
 function beginningPrompt() {
   inquirer
     .prompt([
@@ -45,7 +62,114 @@ function add() {
       },
     ])
     .then((answer) => {
-      console.log(answer);
+      console.log(answer.addingWhat);
+
+      switch (answer.addingwhat) {
+        case `Department`:
+          addDepartment();
+          break;
+        case `Role`:
+          addRole();
+          break;
+        default:
+          addEmployee();
+          break;
+      }
+    });
+}
+
+function addDepartment() {
+  inquirer
+    .prompt([
+      {
+        type: `input`,
+        name: `input`,
+        message: `Enter new Department`,
+      },
+    ])
+    .then((answer) => {
+      connection.query(
+        `INSERT INTO ? (name) VALUE ?`,
+        [shuttle, answer.input],
+        (err, result) => {
+          if (err) throw err;
+          console.log(`input added!`);
+        }
+      );
+    });
+}
+
+function addRole() {
+  connection.query(`SELECT name, id FROM  department`, (err, result) => {
+    if (err) throw err;
+    console.log(`names of departments ${result}`);
+  });
+
+  inquirer
+    .prompt([
+      {
+        type: `input`,
+        name: `title`,
+        message: `Enter new Role title`,
+      },
+      {
+        type: `input`,
+        name: `salary`,
+        message: `Enter Role's salary`,
+      },
+      {
+        type: `list`,
+        name: `department`,
+        message: `Select which department this role is apart of.`,
+        choices: [...result.name],
+      },
+    ])
+    .then((answer) => {
+      connection.query(
+        `INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`,
+        [answer.title, answer.salary, answer.department],
+        (err, result) => {
+          if (err) throw err;
+          console.log(`role added`);
+        }
+      );
+    });
+}
+
+function addEmployee() {
+  connection.query(`SELECT title, id FROM role`, (err, result) => {
+    if (err) throw err;
+    console.log(`roles: ${result.title}`);
+  });
+
+  inquirer
+    .prompt([
+      {
+        type: `input`,
+        name: `firstName`,
+        message: `Enter employee's first name`,
+      },
+      {
+        type: `input`,
+        name: `lastName`,
+        message: `Enter employee's last name`,
+      },
+      {
+        type: `list`,
+        name: `role`,
+        message: `select which role employee would have`,
+        choices: [...result.title],
+      },
+    ])
+    .then((answers) => {
+      connection.query(
+        `INSERT INTO Employee (first_name, last_name, role_id) VALUE (?,?,?)`,
+        [answer.firstName, answer.lastName, answer.role],
+        (err, result) => {
+          if (err) throw err;
+          console.log(`Employee added!`);
+        }
+      );
     });
 }
 
