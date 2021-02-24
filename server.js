477f349689b503ad7143dc5f905cb31b9dda425a
Eric Hunter <ehunter7@live.com>
2021-02-23 22:52:59 -0800

    adding udpate inquirer functions

--- server.js
+++ server.js
@@ -96,89 +96,95 @@ function addDepartment() {
           console.log(`input added!`);
         }
       );
+      beginningPrompt();
     });
-}
-
-function getDepartments() {
-  connection.query(`SELECT * FROM  department`, (err, result) => {
-    if (err) throw err;
-    console.table(result);
-    return result;
-  });
 }
 
 function addRole() {
-  console.log("adding role");
+  connection.query(`SELECT name FROM  department`, (err, result) => {
+    if (err) throw err;
+    inquirer
+      .prompt([
+        {
+          type: `input`,
+          name: `title`,
+          message: `Enter new Role title`,
+        },
+        {
+          type: `input`,
+          name: `salary`,
+          message: `Enter Role's salary`,
+        },
+        {
+          type: `list`,
+          name: `department`,
+          message: `Select which department this role is apart of.`,
+          choices: [...result],
+        },
+      ])
+      .then((answer) => {
+        const depId = toString(answer.department);
+        connection.query(
+          `SELECT id FROM department WHERE name = depId`,
+          (err, res) => {
+            console.log({ res });
 
-  const departments = getDepartments();
-  console.log(departments);
-
-  inquirer
-    .prompt([
-      {
-        type: `input`,
-        name: `title`,
-        message: `Enter new Role title`,
-      },
-      {
-        type: `input`,
-        name: `salary`,
-        message: `Enter Role's salary`,
-      },
-      {
-        type: `list`,
-        name: `department`,
-        message: `Select which department this role is apart of.`,
-        choices: [...result.name],
-      },
-    ])
-    .then((answer) => {
-      connection.query(
-        `INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`,
-        [answer.title, answer.salary, answer.department],
-        (err, result) => {
-          if (err) throw err;
-          console.log(`role added`);
-        }
-      );
-    });
+            connection.query(
+              `INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`,
+              [answer.title, answer.salary, res],
+              (err, result) => {
+                if (err) throw err;
+                console.log(`role added`);
+              }
+            );
+            beginningPrompt();
+          }
+        );
+      });
+  });
 }
 
 function addEmployee() {
-  connection.query(`SELECT title, id FROM role`, (err, result) => {
+  connection.query(`SELECT title FROM role`, (err, result) => {
     if (err) throw err;
-    console.log(`roles: ${result.title}`);
-  });
+    inquirer
+      .prompt([
+        {
+          type: `input`,
+          name: `firstName`,
+          message: `Enter employee's first name`,
+        },
+        {
+          type: `input`,
+          name: `lastName`,
+          message: `Enter employee's last name`,
+        },
+        {
+          type: `list`,
+          name: `role`,
+          message: `select which role employee would have`,
+          choices: [...result],
+        },
+      ])
+      .then((answers) => {
+        connection.query(
+          `SELECT id FROM role WHERE title = ${answers.role}`,
+          (err, res) => {
+            if (err) throw err;
 
-  inquirer
-    .prompt([
-      {
-        type: `input`,
-        name: `firstName`,
-        message: `Enter employee's first name`,
-      },
-      {
-        type: `input`,
-        name: `lastName`,
-        message: `Enter employee's last name`,
-      },
-      {
-        type: `list`,
-        name: `role`,
-        message: `select which role employee would have`,
-        choices: [...result.title],
-      },
-    ])
-    .then((answers) => {
-      connection.query(
-        `INSERT INTO Employee (first_name, last_name, role_id) VALUE (?,?,?)`,
-        [answer.firstName, answer.lastName, answer.role],
-        (err, result) => {
-          if (err) throw err;
-          console.log(`Employee added!`);
-        }
-      );
-    });
+            connection.query(
+              `INSERT INTO Employee (first_name, last_name, role_id) VALUE (?,?,?)`,
+              [answer.firstName, answer.lastName, res],
+              (err, result) => {
+                if (err) throw err;
+                console.log(`Employee added!`);
+              }
+            );
+            beginningPrompt();
+          }
+        );
+      });
+  });
 }
 
 function view() {
@@ -188,11 +194,14 @@ function view() {
         type: `list`,
         name: `viewingWhat`,
         message: `What would you like to view?`,
-        choices: [`Departments`, `Roles`, `Employees`],
+        choices: [`Department`, `Role`, `Employee`],
       },
     ])
     .then((answer) => {
-      console.log(answer);
+      connection.query(`SELECT * FROM ${answer.viewingWhat}`, (err, res) => {
+        console.table(res);
+        beginningPrompt();
+      });
     });
 }
 
@@ -203,12 +212,117 @@ function update() {
         type: `list`,
         name: `updatingWhat`,
         message: `What would you like to update?`,
-        choices: [`Departments`, `Roles`, `Employees`],
+        choices: [`Department`, `Role`, `Employee`],
       },
     ])
     .then((answer) => {
       console.log(answer);
+      beginningPrompt();
     });
 }
+
+function updateDepartment() {
+  connection.query("SELECT name FROM department", (err, result) => {
+    if (err) throw err;
+
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "update",
+          message: "Select a department to udpate",
+          choices: [...result],
+        },
+        {
+          type: "input",
+          name: "updated",
+          message: "Rename department to: ",
+        },
+      ])
+      .then((answer) => {
+        connection.query(
+          "INSERT INTO department (name) WHERE ? VALUE (?)",
+          [{ name: answer.update }, answer.updated],
+          (err, res) => console.log("Department updated!")
+        );
+      });
+  });
+}
+
+function updateRole() {
+  connection.query("SELECT title FROM role", (err, result) => {
+    if (err) throw err;
+
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "update",
+          message: "Select which role to udpate",
+          choices: [...result],
+        },
+        {
+          type: "input",
+          name: "updated",
+          message: "Rename role title to: ",
+        },
+        {
+          type: "input",
+          name: "updatedSalary",
+          message: "Update roles salary: ",
+        },
+      ])
+      .then((answer) => {
+        connection.query(
+          "INSERT INTO role (title, salary) WHERE ? VALUE (?, ?)",
+          [{ title: answer.update }, answer.updated, answer.updatedSalary],
+          (err, res) => console.log("Role updated!")
+        );
+      });
+  });
+}
+
+function updateEmployee() {
+  connection.query("SELECT first_name FROM employee", (err, result) => {
+    if (err) throw err;
+    connection.query("SELECT title FROM role", (err, roleList) => {
+      if (err) throw err;
+
+      inquirer
+        .prompt([
+          {
+            type: "list",
+            name: "update",
+            message: "Select an Employee to udpate",
+            choices: [...result],
+          },
+          {
+            type: "input",
+            name: "updatedFirst",
+            message: "Update first name: ",
+          },
+          {
+            type: "input",
+            name: "updatedLast",
+            message: "Update last name: ",
+          },
+          {
+            type: "list",
+            name: "updatedRole",
+            message: "Select Employee's role",
+            choices: [...roleList],
+          },
+        ])
+        .then((answer) => {
+          connection.query(
+            "INSERT INTO employee (first_name, last_name, role) WHERE ? VALUE (?, ?, ?)",
+            [{ first_name: answer.update },  answer.updatedFirst, answer.updatedLast, answer.updated],
+            (err, res) => console.log("Employee updated!")
+          );
+        });
+    });
+  });
+}
+//TODO create delete functions
 
 beginningPrompt();
